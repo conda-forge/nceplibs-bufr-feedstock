b94a1c518435a1fed3ffad89eda9f491b85ba58b
darothen <daniel@danielrothenberg.com>
2024-05-23 14:15:50 -0600

    Add cross-compilation guard for ctest

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,18 +10,21 @@ cmake \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DENABLE_PYTHON=ON \
     -DBUILD_SHARED_LIBS=ON \
-    "${CMAKE_ARGS}" \
     "${SRC_DIR}"
 
 make -j"${CPU_COUNT}"
 make install
 
-# NOTE: On osx64, disable tests that fail when building shared libraries. As a
-# kluge we check an env var set by the conda-forge build process to detect this.
-if [[ -z "${OSX_ARCH}" ]]; then
-    ctest --output-on-failure -j"${CPU_COUNT}"
-else
-    echo "Building on OSX; disabling known failing/flaky tests."
-    ctest --output-on-failure -j"${CPU_COUNT}" \
-        -E "test_pyncepbufr_write|intest(7|10|12)_*|outtest(1|4|9|10)_*|test_debufr"
+# NOTE: Guard against testing during cross-compilation builds, as the compiled
+# binaries will typically fail.
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+    # NOTE: On osx64, disable tests that fail when building shared libraries. As a
+    # kluge we check an env var set by the conda-forge build process to detect this.
+    if [[ -z "${OSX_ARCH}" ]]; then
+        ctest --output-on-failure -j"${CPU_COUNT}"
+    else
+        echo "Building on OSX; disabling known failing/flaky tests."
+        ctest --output-on-failure -j"${CPU_COUNT}" \
+            -E "test_pyncepbufr_write|intest(7|10|12)_*|outtest(1|4|9|10)_*|test_debufr"
+    fi
 fi
